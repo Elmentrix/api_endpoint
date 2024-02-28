316baacb54cdb02ab7b411333133bd00259a1027
Emmanuel Laryea <laryeaonyx@gmail.com>
2024-02-28 02:31:24 +0000

    serializer working

--- sheetData/sheet/views.py
+++ sheetData/sheet/views.py
@@ -1,17 +1,30 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from .serializers import itemsSerializer
 from .key import key_log_read
+from .models import items
 
-# Create your views here.
-def home(request):
-    return HttpResponse("key_log_read")
+
+# # sheet data
+# data = key_log_read()
+# for row in data[1:]:
+#         print(str(row(0)))
+
+# # reading from excel file to db
+# def home(request):
+#     # loop through data
+    
+        
+#     return HttpResponse("key_log_read")
 
 def itemSel(request):
-
     # tasks
     # 1. get all items from db
     # 2. serialize them
     # 3. return json 
 
-    return HttpResponse()
+    # getting all items from the db and passing it through serializer
+    items_data = items.objects.all()
+    serial = itemsSerializer(items_data, many=True)
+
+    return JsonResponse(serial.data, safe=False)
